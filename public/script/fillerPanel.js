a5dd0246b57c03d34c3366b08619d277233ed652
tu pham <shammer280@gmail.com>
2019-11-30 21:43:57 +0700

    update views

--- public/script/fillerPanel.js
+++ public/script/fillerPanel.js
@@ -1,7 +1,2 @@
-$('#sectionChooser').change(function(){
-  var myID = $(this).val();
-  $('.panel').each(function(){
-      myID === $(this).attr('id') ? $(this).show() : $(this).hide();
-  });
-});
+
 
